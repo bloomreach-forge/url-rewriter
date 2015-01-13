999bed8fdb6f7e0ad5e249f8e0fc50311218c75b
Bartosz Oudekerk <bartosz.oudekerk@bloomreach.com>
2015-01-13 16:17:35 +0000

    Add ping-servlet to skippedprefixes default value

--- urlrewriter-plugin-site/src/main/java/org/onehippo/forge/rewriting/UrlRewriteConstants.java
+++ urlrewriter-plugin-site/src/main/java/org/onehippo/forge/rewriting/UrlRewriteConstants.java
@@ -35,7 +35,7 @@ public class UrlRewriteConstants {
     public static final String SKIP_POST_PROPERTY = "urlrewriter:skippost";
     public static final Boolean SKIP_POST_PROPERTY_DEFAULT_VALUE = true;
     public static final String SKIPPED_PREFIXES_PROPERTY = "urlrewriter:skippedprefixes";
-    public static final String[] SKIPPED_PREFIXES_DEFAULT_VALUE = new String[]{"/_cmsrest","/_cmsinternal","/_rp","/_hn:"};
+    public static final String[] SKIPPED_PREFIXES_DEFAULT_VALUE = new String[]{"/_cmsrest","/_cmsinternal","/_rp","/_hn:","/ping/"};
     public static final String FROM_PROPERTY = "urlrewriter:rulefrom";
     public static final String TO_PROPERTY = "urlrewriter:ruleto";
     public static final String TYPE_PROPERTY = "urlrewriter:ruletype";
